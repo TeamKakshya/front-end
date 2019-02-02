8bac5d6f19b1a3d0cbf2a622084ee46f20c5561d
Ranjan <074bex435.ranjan@pcampus.edu.np>
2019-02-02 23:32:57 +0545

    Same as notice done for add events but last until reload

--- main.js
+++ main.js
@@ -22,7 +22,8 @@ $(document).ready(function() {
                 $(this).addClass('active');
             });
         }
-    });
+    });//for the tab panel widget scroll down on click
+
     function change() {
       document.getElementsByClassName('options').style.display="block";
     }
@@ -31,20 +32,35 @@ $(document).ready(function() {
     $('.add_notice').on('click',show_add);
     function show_add() {
     $(".add_input").css("display","block");
-  }
+  }//show show hidden input text for add notice
 
-    //for taking the value of input text in input add events
+    //for taking the value of input text in input add events and add it in notice marquee
     $('.add_button').on('click',takevalue);
     function takevalue() {
       var test = $("#notice_input").val();
       $('.mark').append('<a href="#" target="_blank">'+test);
-    }
+    }//for taking the value of input text in input add events and add it in notice marquee
 
+    //show hidden input text for events
+    $('.event_addbtn').on('click',show_eventadd);
+    function show_eventadd() {
+    $(".events_form").css("display","block");
+  }//show show hidden input text for add notice
+
+    //for taking the value of input text in input add events and add it in notice marquee
+    $('.event_donebtn').on('click',take_event);
+    function take_event() {
+      var test_event = $(".event_text").val();
+      $('.events_only').append('<ul class="event 3"><a href="#"><li class="eventProg">test_event</li><li class="eventDay">@4pm</li></a></ul>');
+    }//for taking the value of input text in input add events and add it in notice marquee
+
+
+
+    //for chat toggles i.e from up to down
     $('.bUTTON').on('click',change_css );
-
     function change_css() {
         $('.collapse').slideToggle(200);
-      }
+      }    //for chat toggles i.e from up to down
 
     //chat width fixed of tABS
 
